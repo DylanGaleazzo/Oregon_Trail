9afa466a724cc62162948a5bb6516e2cc4d0e72b
Anson Biggs <ansonleebiggs@gmail.com>
2019-11-13 21:49:26 -0700

    merge

--- Oregon_Trail/Landmark.h
+++ Oregon_Trail/Landmark.h
@@ -7,48 +7,21 @@ class LandMark
 private:
 	Wagon wagon;
 	string name;
-<<<<<<< HEAD
-	int miles;
-	virtual int isFort() = 0;
-};
-
-class Fort:public Landmark
-{
-public:
-	Fort(string name, int miles, Wagon wagon);
-	ostream& operator<<(ostream& os);
-	int is_Fort() { return 1; }
-=======
 	Date date;
 public:
 	string get_Name();
 	Wagon get_Wagon();
 	virtual bool isFort();
->>>>>>> parent of fe36c92... Merge branch 'master' of https://github.com/DylanGaleazzo/Oregon_Trail
 };
 
 class Fort:public LandMark
 {
 public:
-<<<<<<< HEAD
 	River(string name, int miles, Wagon wagon);
 	ostream& operator<<(ostream& os);
 	int is_Fort() { return 2; }
-=======
-	ostream& operator<<(ostream& os);
-	bool isFort() { return true; }
->>>>>>> parent of fe36c92... Merge branch 'master' of https://github.com/DylanGaleazzo/Oregon_Trail
 };
 
 class River:public LandMark
 {
 public:
-<<<<<<< HEAD
-	Other(string name, int miles, Wagon wagon);
-	ostream& operator<<(ostream& os);
-	int is_Fort() { return 3; }
-=======
-	ostream& operator<<(ostream& os);
-	bool isFort() { return false; }
->>>>>>> parent of fe36c92... Merge branch 'master' of https://github.com/DylanGaleazzo/Oregon_Trail
-};
